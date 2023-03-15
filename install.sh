57b0d7bfd7a1ed13e0b414cacf6fb7cb29782261
Teagan glenn <teagan.m.glenn@gmail.com>
2023-03-14 22:58:29 -0600

    Clean up npms

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 npm -f i
 cd apps
-cd claim-service && npm -f i && cd ..
-cd device-service && npm -f i && cd ..
-cd gateway && npm -f i && cd ..
-cd user-service && npm -f i && cd ..
-cd ../libs/core && npm -f i
+cd claim-service && npm prune && npm -f i && cd ..
+cd device-service && npm prune && npm -f i && cd ..
+cd gateway && npm prune && npm -f i && cd ..
+cd user-service && npm prune && npm -f i && cd ..
+cd ../libs/core && npm prune && npm -f i
