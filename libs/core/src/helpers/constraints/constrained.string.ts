896ad03aa22dc54334a20b5706eef0bda7ec7fce
Teagan glenn <teagan.m.glenn@gmail.com>
2023-03-14 23:15:25 -0600

    More strings

--- libs/core/src/helpers/constraints/constrained.string.ts
+++ libs/core/src/helpers/constraints/constrained.string.ts
@@ -30,5 +30,62 @@ ZodString.prototype.regex = describeAndApply(
     ZodString.prototype.regex,
     (regex, message) => `Must match ${escapeRegExp(regex)}.`,
 );
+ZodString.prototype.email = describeAndApply(
+    ZodString.prototype.email,
+    (message) => `Must be a valid email address.`,
+);
+ZodString.prototype.url = describeAndApply(
+    ZodString.prototype.url,
+    (message) => `Must be a valid URL.`,
+);
+ZodString.prototype.emoji = describeAndApply(
+    ZodString.prototype.emoji,
+    (message) => `Must contain an emoji.`,
+);
+ZodString.prototype.uuid = describeAndApply(
+    ZodString.prototype.uuid,
+    (message) => `Must be a valid UUID.`,
+);
+ZodString.prototype.cuid = describeAndApply(
+    ZodString.prototype.cuid,
+    (message) => `Must be a valid CUID.`,
+);
+ZodString.prototype.cuid2 = describeAndApply(
+    ZodString.prototype.cuid2,
+    (message) => `Must be a valid CUID2.`,
+);
+ZodString.prototype.ulid = describeAndApply(
+    ZodString.prototype.ulid,
+    (message) => `Must be a valid ULID.`,
+);
+ZodString.prototype.ip = describeAndApply(
+    ZodString.prototype.ip,
+    (options?: string | { version?: 'v4' | 'v6'; message?: string }) => `Must be a valid IP.`,
+);
+ZodString.prototype.datetime = describeAndApply(
+    ZodString.prototype.datetime,
+    (
+        options?:
+            | string
+            | {
+          message?: string | undefined;
+          precision?: number | null;
+          offset?: boolean
+        }) => `Must be a valid datetime.`,
+);
+ZodString.prototype.includes = describeAndApply(
+    ZodString.prototype.includes,
+    (
+        value: string,
+        options?: { message?: string; position?: number }) => `Must include the substring ${value}.`,
+);
+ZodString.prototype.startsWith = describeAndApply(
+    ZodString.prototype.startsWith,
+    (value, message) => `Must be begin with the string ${value}.`,
+);
+ZodString.prototype.endsWith = describeAndApply(
+    ZodString.prototype.endsWith,
+    (value, message) => `Must be end with the string ${value}.`,
+);
 
 export const stringConstraints = z.string;
